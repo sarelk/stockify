f5fa5d21b7f10fd8746bd6356183f5a54fb948d0
Sarel <Sarelk@gmail.com>
2020-05-22 13:10:08 +0300

    search input added

--- stockify-app/src/App.vue
+++ stockify-app/src/App.vue
@@ -1,16 +1,21 @@
 <template>
   <div id="app">
     <h1>Stockify</h1>
+          <v-text-field
+            label="Search Stock"
+            placeholder="AAPL/SHOP..."
+            regular
+          ></v-text-field>
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css2?family=Bungee+Inline&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Bungee+Inline&display=swap");
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -33,7 +38,7 @@
 }
 
 h1 {
-  font-family: 'Bungee Inline', cursive;
-  font-size:56px;
+  font-family: "Bungee Inline", cursive;
+  font-size: 56px;
 }
 </style>
--- stockify-app/src/views/Home.vue
+++ stockify-app/src/views/Home.vue
@@ -1,24 +1,21 @@
 <style scoped>
-* { display: flex;justify-content: center; }
+* { display: flex;justify-content: center;}
 html { background:black; }
 </style>
 
 <template>
   <div class="home">
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
     <SingleStock/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-// import HelloWorld from '@/components/HelloWorld.vue'
 import SingleStock from '@/components/SingleStock.vue'
 
 export default {
   name: 'Home',
   components: {
-    // HelloWorld,
     SingleStock
   }
 }
